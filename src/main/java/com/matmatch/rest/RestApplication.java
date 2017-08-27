1f966780df7a2082302242840017d9fecd16fb7a
Mustafa Zidan <mustafa@zidan.me>
2017-08-27 14:38:53 +0200

    initialize gradle spring application project
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

---
+++ src/main/java/com/matmatch/rest/RestApplication.java
@@ -0,0 +1,16 @@
+package com.matmatch.rest;
+
+import com.matmatch.rest.config.ApplicationProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+}
